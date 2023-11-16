61d40ed1eeb61165ba23bcfa497f4f1b952121d4
Ranjan Dutta <connecttoranjan@gmail.com>
2023-11-16 12:39:37 +0530

    border working

--- lib/Compnent/Blast_furnace.dart
+++ lib/Compnent/Blast_furnace.dart
@@ -419,21 +419,58 @@ class ColState extends State<Col> {
                       width: 2.0, // Border width
                     ),
                   ),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 3),
-                  child: const Row(
+                  // padding:
+                  //     const EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        "Parameter",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 152, 152, 152)),
+                      Expanded(
+                        flex: 7,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              right: BorderSide(
+                                color: Color.fromARGB(113, 44, 129, 227),
+                                width: 2.0,
+                              ),
+                            ),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 3),
+                          child: const Text(
+                            "Parameter",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 152, 152, 152)),
+                          ),
+                        ),
                       ),
-                      Text("Value",
-                          style: TextStyle(
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          decoration: const BoxDecoration(),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 3),
+                          child: const Text(
+                            "Value",
+                            style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 152, 152, 152))),
+                              color: Color.fromARGB(255, 152, 152, 152),
+                            ),
+                            textAlign: TextAlign.right,
+                          ),
+                        ),
+                      ),
+                      // Text(
+                      //   "Parameter",
+                      //   style: TextStyle(
+                      //       fontWeight: FontWeight.bold,
+                      //       color: Color.fromARGB(255, 152, 152, 152)),
+                      // ),
+                      // Text("Value",
+                      //     style: TextStyle(
+                      //         fontWeight: FontWeight.bold,
+                      //         color: Color.fromARGB(255, 152, 152, 152),),),
                     ],
                   ),
                 ),
@@ -473,11 +510,11 @@ Widget _row(h, d, color, i) {
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
     children: [
       Expanded(
-        flex: 8,
+        flex: 7,
         child: Container(
           decoration: const BoxDecoration(),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 5),
             child: Text(
               h,
               style: TextStyle(color: color),
@@ -490,6 +527,12 @@ Widget _row(h, d, color, i) {
               flex: 3,
               child: Container(
                 decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(
+                        color: Color.fromARGB(113, 44, 129, 227),
+                        width: 2.0,
+                      ),
+                    ),
                     color: double.parse(d) >= 1.6 && double.parse(d) <= 1.65
                         ? Colors.amber
                         : double.parse(d) > 1.65
