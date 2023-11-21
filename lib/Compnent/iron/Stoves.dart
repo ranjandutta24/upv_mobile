f6e7b9dbe82d0389a85aa01252e0fb80787914ba
AnamikaTubi <anamikatubi@gmail.com>
2023-11-21 11:56:14 +0530

    i

--- lib/Compnent/iron/Stoves.dart
+++ lib/Compnent/iron/Stoves.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:upv_mobile/Services/service_page.dart';
 
 class Stoves extends StatefulWidget {
   const Stoves({super.key});
@@ -13,6 +17,88 @@ class StovesState extends State<Stoves> {
   late dynamic stovesData;
   var loading = false;
   var num = -1;
+
+  List<dynamic> rows = [];
+
+  fun() {
+    TechnoService();
+    var duration = const Duration(seconds: 5);
+    Timer.periodic(duration, (Timer timer) {
+      TechnoService();
+    });
+  }
+
+  TechnoService() async {
+    await stoveService().then((data) {
+      if (mounted) {
+        print(data.body);
+        setState(() {
+          stovesData = json.decode(data.body);
+          rows = [
+            {
+              "head": "Stv 1 Gas Flow [Nm3/hr]",
+              "data1": stovesData["EX2"].toString(),
+              "data2": stovesData["EX2"].toString(),
+              "selected": false,
+              "i": 0,
+            },
+            {
+              "head": "Stv 1 Dome Temp. [DegC]",
+              "data1": stovesData["EX2"].toString(),
+              "data2": stovesData["EX2"].toString(),
+              "selected": false,
+              "i": 1,
+            },
+            {
+              "head": "Stv 2 Gas Flow [Nm3/hr]",
+              "data1": stovesData["EX2"].toString(),
+              "data2": stovesData["EX2"].toString(),
+              "selected": false,
+              "i": 2,
+            },
+            {
+              "head": "Stv 2 Dome Temp. [DegC]",
+              "data1": stovesData["EX2"].toString(),
+              "data2": stovesData["EX2"].toString(),
+              "selected": false,
+              "i": 3,
+            },
+            {
+              "head": "Stv 3 Gas Flow [Nm3/hr]",
+              "data1": stovesData["EX2"].toString(),
+              "data2": stovesData["EX2"].toString(),
+              "selected": false,
+              "i": 4,
+            },
+            {
+              "head": "Stv 3 Dome Temp.[DegC]",
+              "data1": stovesData["EX2"].toString(),
+              "data2": stovesData["EX2"].toString(),
+              "selected": false,
+              "i": 5,
+            },
+          ];
+          if (num != -1) {
+            rows[num]["selected"] = true;
+          }
+          loading = false;
+        });
+      } else {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            duration: const Duration(seconds: 3),
+            content: const Center(
+              child: Text('Login Failed, wrong userid or password'),
+            ),
+            action: SnackBarAction(label: '', onPressed: () {}),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
